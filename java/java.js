bbea2598c7ed82469dc8fdc43a6074cec7be7672
henryarcher <henryarcher007@gmail.com>
2022-10-16 23:13:49 +0100

    rewrote js to include function parameters, and added DOM manipulation

--- java/java.js
+++ java/java.js
@@ -1,15 +1,5 @@
 
-//global variables
-let round = 0;
-let games = 0;
-
-let computer = 0;
-let you = 0;
-
-let computerSelection = 0;
-let playerSelection = 0
-
-//playRound();
+getPlayerChoice();
 
 function getComputerChoice(){
         
@@ -32,94 +22,93 @@ function getComputerChoice(){
 };
 
 function getPlayerChoice(){
+   
+    let rock = document.getElementById('rock')
+    let paper = document.getElementById('paper')
+    let scissor = document.getElementById('scissor')
+    let round = 1
 
+
+    rock.addEventListener('click', () =>{
+        playRound("rock", getComputerChoice() )
+        roundCount(round ++);
+    });
+
+    paper.addEventListener('click', () =>{
+        playRound("paper", getComputerChoice())
+        roundCount(round ++);
+    });
+    
+    scissor.addEventListener('click', () =>{
+        playRound("scissor", getComputerChoice())
+        roundCount(round ++);
+    });
 }
 
-function rockPaperScissors(){
+let you = 0
+let computer = 0
 
+function playRound(playerSelection, computerSelection){
+    let message = 0
     if(playerSelection == computerSelection){
-        return message = console.log(`Tie! You and the computer selected ${playerSelection}`)
+        displayPlayRound(`Tie! You and the computer selected ${playerSelection}`)
     }
     else if(playerSelection == 'rock' && computerSelection == 'paper'){
         computer ++;
-        return message = console.log("You lose! Paper beats Rock.")
+        displayPlayRound("You lose! Paper beats Rock.")
     }
     else if(playerSelection == 'paper' && computerSelection == 'scissor'){
         computer ++;
-        return message = console.log("You lose! Scissors beats Paper.")
+        displayPlayRound("You lose! Scissors beats Paper.")
     }
     else if(playerSelection == 'scissor' && computerSelection == 'rock'){
         computer ++;
-        return message = console.log("You lose! Rock beats Scissors.")
+        displayPlayRound("You lose! Rock beats Scissors.")
     }
     else if(computerSelection == 'scissor' && playerSelection == 'rock'){
         you ++;
-        return message = console.log("You win! Rock beats Scissors.")
+        displayPlayRound("You win! Rock beats Scissors.")
     }
     else if(computerSelection == 'rock' && playerSelection == 'paper'){
         you ++;
-        return message = console.log("You Win! Paper beats Scissors.")
+        displayPlayRound("You Win! Paper beats Scissors.")
     }
     else if(computerSelection == 'paper' && playerSelection == 'scissor'){
         you ++;
-        return message = console.log("You Win! Scissors beats Paper.")
+        displayPlayRound("You Win! Scissors beats Paper.")
     }
+    
 };
 
-function game(){
-
-    // rockPaperScissors function
-
-    let getPlayerChoice = prompt("Rock, Paper, or Scissor?");
-    playerSelection = getPlayerChoice.toLowerCase();
-
-    computerSelection = getComputerChoice();
-
-    rockPaperScissors();
-
-};
+function displayPlayRound(message){
+    const statement = document.querySelector('#statement')
+    statement.textContent = message
+}
 
 
-/*
-// loop function
-function playRound(){
-    for (let i = 1; i < 6; i++) {
-    game()
-    console.log(i);
+function roundCount(round){
+    displayRound();
+    
+    if (round >= 5){
+        console.log("GAME OVER")
+        winnerLoser(you, computer)
+    }
+    function displayRound(){
+        if (round < 6){
+            const rounds = document.querySelector('#rounds');
+            rounds.textContent = `Round ${round};`
+        }
     };
-    if (i = 5)
-    return winnerLoser()
-};
-*/
+}
+
 
 
 // winner/loser declarer
-function winnerLoser(){
+function winnerLoser(you, computer){
 if (you > computer){
     return console.log('You win the game!')
-}
+}   
 else{
     return console.log('You lose the game!')
 }
 };
-
-getPlayerChoice()
-
-function getPlayerChoice(){
-    const rock = document.getElementById('rock');
-    const paper = document.getElementById('paper');
-    const scissor = document.getElementById('scissor');
-
-    rock.addEventListener('click', function onClick(){
-        console.log('rock clicked');
-    });
-
-    paper.addEventListener('click', function onClick(){
-        console.log('paper clicked');
-    });
-
-    scissor.addEventListener('click', function onClick(){
-        console.log('scissor clicked');
-    });
-
-}
